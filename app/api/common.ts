3e26fbffb1da4e01c072274a1ff4a66169761945
aforwardz <aforwardzyu@gmail.com>
2023-07-10 15:52:02 +0800

    use another way to count tokens

--- app/api/common.ts
+++ app/api/common.ts
@@ -1,4 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
+import { OpenaiPath, CostWay } from "@/app/constant";
+import { user_cli } from "./db";
+import { get_encoding, encoding_for_model } from "tiktoken";
+const { Tiktoken } = require("tiktoken/lite");
+const cl100k_base = require("tiktoken/encoders/cl100k_base.json");
 
 export const OPENAI_URL = "api.openai.com";
 const DEFAULT_PROTOCOL = "https";
@@ -6,7 +11,73 @@ const PROTOCOL = process.env.PROTOCOL ?? DEFAULT_PROTOCOL;
 const BASE_URL = process.env.BASE_URL ?? OPENAI_URL;
 const DISABLE_GPT4 = !!process.env.DISABLE_GPT4;
 
-export async function requestOpenai(req: NextRequest) {
+const ALLOWD_COST = new Set(Object.values(CostWay));
+
+const DEFAULT_COST_WAY = "balance";
+const COST_WAY = !ALLOWD_COST.has(process.env.COST_WAY ?? "")
+  ? DEFAULT_COST_WAY
+  : process.env.COST_WAY;
+
+const encoder = new Tiktoken(
+  cl100k_base.bpe_ranks,
+  cl100k_base.special_tokens,
+  cl100k_base.pat_str,
+);
+
+async function costBalance(
+  res: Response,
+  hashCode: string,
+  model: string,
+  extraTokens: number,
+) {
+  // cost count
+  if (COST_WAY === CostWay.UseCount) {
+    user_cli.useModelCount(hashCode, model);
+    return;
+  }
+
+  // cost tokens
+  let completionTokens = 0;
+  const reader = res.body?.getReader();
+  if (!reader) {
+    console.error("Error: fail to read data from response");
+    return;
+  }
+
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) {
+      break;
+    }
+
+    const textDecoder = new TextDecoder("utf-8");
+    const chunk = textDecoder.decode(value);
+
+    let deltaText = "";
+    for (const line of chunk.split("\n")) {
+      const trimmedLine = line.trim();
+      if (!trimmedLine || trimmedLine === "data: [DONE]") {
+        continue;
+      }
+
+      const json = trimmedLine.replace("data: ", "");
+      const obj = JSON.parse(json);
+      const content = obj.choices ? obj.choices[0].delta.content : "";
+      deltaText = deltaText.concat(content);
+    }
+
+    completionTokens += encoder.encode(deltaText).length;
+    console.log("[OpenAI] completion tokens: ", completionTokens);
+    encoder.free;
+  }
+
+  if (COST_WAY === CostWay.UseBalance) {
+    const totalTokens = completionTokens + extraTokens;
+    user_cli.useModelBalance(hashCode, model, totalTokens);
+  }
+}
+
+export async function requestOpenai(req: NextRequest, hashCode: string) {
   const controller = new AbortController();
   const authValue = req.headers.get("Authorization") ?? "";
   const openaiPath = `${req.nextUrl.pathname}${req.nextUrl.search}`.replaceAll(
@@ -48,6 +119,23 @@ export async function requestOpenai(req: NextRequest) {
     signal: controller.signal,
   };
 
+  const modelName = req.headers.get("ModelName") ?? "";
+  let promptTokens = 0;
+
+  if (COST_WAY === CostWay.UseBalance && req.body) {
+    const clonedBody = await req.text();
+    const jsonBody = JSON.parse(clonedBody);
+
+    if (jsonBody?.messages.length > 0 && jsonBody?.messages.length <= 6) {
+      const lastMessage = jsonBody.messages[jsonBody.messages.length - 1];
+      if (lastMessage.role === "user") {
+        promptTokens += encoder.encode(lastMessage.content).length;
+        // encoder.free()
+      }
+    }
+  }
+  console.log("[OpenAI] prompt tokens: ", promptTokens);
+
   // #1815 try to refuse gpt4 request
   if (DISABLE_GPT4 && req.body) {
     try {
@@ -82,59 +170,9 @@ export async function requestOpenai(req: NextRequest) {
     // to disbale ngnix buffering
     newHeaders.set("X-Accel-Buffering", "no");
 
-    return new Response(res.body, {
-      status: res.status,
-      statusText: res.statusText,
-      headers: newHeaders,
-    });
-  } finally {
-    clearTimeout(timeoutId);
-  }
-}
-
-export async function requestPocketAI(req: NextRequest) {
-  const controller = new AbortController();
-  const authValue = req.headers.get("Authorization") ?? "";
-  const openaiPath = `${req.nextUrl.pathname}${req.nextUrl.search}`.replaceAll(
-    "/api/pocket/",
-    "",
-  );
-
-  let baseUrl = BASE_URL;
-
-  if (!baseUrl.startsWith("http")) {
-    baseUrl = `${PROTOCOL}://${baseUrl}`;
-  }
-
-  console.log("[Base Url]", baseUrl);
-
-  const timeoutId = setTimeout(() => {
-    controller.abort();
-  }, 10 * 60 * 1000);
-
-  const fetchUrl = `${baseUrl}/${openaiPath}`;
-  const fetchOptions: RequestInit = {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: authValue,
-    },
-    cache: "no-store",
-    method: req.method,
-    body: req.body,
-    // @ts-ignore
-    duplex: "half",
-    signal: controller.signal,
-  };
-
-  try {
-    const res = await fetch(fetchUrl, fetchOptions);
-
-    // to prevent browser prompt for credentials
-    const newHeaders = new Headers(res.headers);
-    newHeaders.delete("www-authenticate");
-
-    // to disbale ngnix buffering
-    newHeaders.set("X-Accel-Buffering", "no");
+    if (openaiPath === OpenaiPath.ChatPath && res.ok) {
+      costBalance(res.clone(), hashCode, modelName, promptTokens);
+    }
 
     return new Response(res.body, {
       status: res.status,
--- app/api/openai/[...path]/route.ts
+++ app/api/openai/[...path]/route.ts
@@ -1,17 +1,10 @@
-import { OpenaiPath, CostWay } from "@/app/constant";
+import { OpenaiPath } from "@/app/constant";
 import { prettyObject } from "@/app/utils/format";
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "../../auth";
 import { requestOpenai } from "../../common";
-import { user_cli } from "../../db";
 
 const ALLOWD_PATH = new Set(Object.values(OpenaiPath));
-const ALLOWD_COST = new Set(Object.values(CostWay));
-
-const DEFAULT_COST_WAY = "balance";
-const COST_WAY = !ALLOWD_COST.has(process.env.COST_WAY ?? "")
-  ? DEFAULT_COST_WAY
-  : process.env.COST_WAY;
 
 async function handle(
   req: NextRequest,
@@ -38,7 +31,6 @@ async function handle(
     );
   }
 
-  const modelName = req.headers.get("ModelName") ?? "";
   const authResult = await auth(req);
   console.log("[Auth Res]", authResult);
   if (authResult.error && authResult.authType === "access") {
@@ -53,33 +45,7 @@ async function handle(
   }
 
   try {
-    const res = await requestOpenai(req);
-
-    if (res.status === 200) {
-      const clonedRes = res.clone();
-      const jsonBody = await clonedRes.json();
-      console.log("[clone Json]", jsonBody);
-      // const jsonBody = JSON.parse(clonedBody);
-
-      if (
-        subpath === OpenaiPath.ChatPath &&
-        (jsonBody?.choices ?? []).length > 0 &&
-        jsonBody.choices[0].finish_reason === "stop"
-      ) {
-        if (COST_WAY === CostWay.UseBalance) {
-          const tokenUsage = jsonBody.usage;
-          user_cli.useModelBalance(
-            authResult.hashCode ?? "",
-            modelName,
-            tokenUsage.total_tokens,
-          );
-        }
-
-        if (COST_WAY === CostWay.UseCount) {
-          user_cli.useModelCount(authResult.hashCode ?? "", modelName);
-        }
-      }
-    }
+    const res = await requestOpenai(req, authResult.hashCode ?? "");
 
     return res;
   } catch (e) {
